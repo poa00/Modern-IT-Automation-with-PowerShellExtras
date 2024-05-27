f5dfb576b632b51aa691a7ba454059ce10f956dc
Nicholas Bissell <thefreeman193@hotmail.com>
2024-05-27 21:23:46 +0100

    Script signing example 12 update

--- Edition-01/Examples/Script Signing/Example 12.ps1
+++ Edition-01/Examples/Script Signing/Example 12.ps1
@@ -2,5 +2,5 @@ $Params = @{
     Path = ${Env:ProgramFiles(x86)}
     ChildPath = 'Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe'
 }
-$SignToolPath = Join-Path @$Params
+$SignToolPath = Join-Path @Params
 & $SignToolPath verify /pa C:\test.ps1
